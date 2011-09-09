4f86546f2df3b2ee68001dac408904b058ec7392
Jason Whitaker <jwhitaker@thewhitakers.ca>
2011-09-09 10:05:01 -0400

    Initial addition of experiments

---
+++ TheWhitakers.WiXSample/TheWhitakers.WiXSample.HelloWorld/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TheWhitakers.WiXSample.HelloWorld
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.exitButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(67, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Hello World";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// exitButton
+			// 
+			this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.exitButton.Location = new System.Drawing.Point(61, 48);
+			this.exitButton.Name = "exitButton";
+			this.exitButton.Size = new System.Drawing.Size(75, 23);
+			this.exitButton.TabIndex = 1;
+			this.exitButton.Text = "E&xit";
+			this.exitButton.UseVisualStyleBackColor = true;
+			this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.exitButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.exitButton;
+			this.ClientSize = new System.Drawing.Size(199, 93);
+			this.Controls.Add(this.exitButton);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Hello World Sample";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button exitButton;
+	}
+}
+
---
+++ TheWhitakers.WiXSample/TheWhitakers.WiXSample.HelloWorld/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TheWhitakers.WiXSample.HelloWorld
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void exitButton_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
